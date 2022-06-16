a780521b9bb98947eed44dbfc3402b9ac99fac95
sepehr niki <sepehrniki@gmail.com>
2022-06-16 22:29:02 +0430

    assignment 2 finished with two models. one with keyup listener one with click submit btn. added cursor pointer on btn

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+const inputElement = document.querySelector('#input');
+const sentence = document.querySelector('.hero-title');
+const submitButton = document.querySelector('.submit');
+
+submitButton.addEventListener('click', changeLocation);
+
+// inputElement.addEventListener('keyup', changeLocation);
+
+function changeLocation() {
+    const location = sentence.querySelector("span");
+    if (inputElement.value.length === 0) {
+        return location.innerText = " Beach";
+    }
+    return location.innerText = inputElement.value;
+}
